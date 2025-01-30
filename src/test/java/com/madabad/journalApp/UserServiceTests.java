1c3031dd404118c83f3e761f9068091cc8a8f397
Adityamad <aadim2999@gmail.com>
2025-01-30 09:41:10 +0530

    disabled tests

--- src/test/java/com/madabad/journalApp/UserServiceTests.java
+++ src/test/java/com/madabad/journalApp/UserServiceTests.java
@@ -2,6 +2,7 @@ package com.madabad.journalApp;
 
 import com.madabad.journalApp.repository.UserRepository;
 import com.madabad.journalApp.services.UserServices;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@Disabled
 public class UserServiceTests {
 
     @Autowired
